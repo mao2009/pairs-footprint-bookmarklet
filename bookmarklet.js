1507f708492eb4f520902bedc281cfc21844d520
mao2009 <en>
2019-05-24 22:04:20 +0900

    fix typo

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 const interval = 1000;
 setInterval(() => {
-            const elem = pairsTab.getElementsByClassName("action_pager_next")[0].getElementsByTagName("a");
+            const elem = document.getElementsByClassName("action_pager_next")[0].getElementsByTagName("a");
             elem[0].click();
         },interval);
