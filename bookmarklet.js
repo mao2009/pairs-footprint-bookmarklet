fd743d5ccb3c6059578d92515037b88b42036370
mao2009 <en>
2019-05-25 10:39:34 +0900

    add index.html

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,13 @@
-const interval = 1000;
-const ID = setInterval(() => {
-            const elem = document.getElementsByClassName("action_pager_next")[0].getElementsByTagName("a");
-            if(elem === undefined){
-                clearInterval(ID)
-            }else{
-                elem[0].click();
-            }
-        },interval);
+const bookmarklet = (()=>{
+    const interval = 1000;
+    const ID = setInterval(() => {
+        const elem = document.getElementsByClassName("action_pager_next")[0].getElementsByTagName("a");
+        if(elem === undefined){
+            clearInterval(ID);
+        }else{
+            elem[0].click();
+        }
+    },interval);
+}).toString().replace(/\s+/g," ").replace("\n","");
+
+document.getElementById("bookmarklet").href = `javascrpt:(${bookmarklet})()`;
