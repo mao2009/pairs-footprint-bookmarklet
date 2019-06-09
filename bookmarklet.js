3f46dba068e7a11f31080cb03a9851ee22237654
mao2009 <en>
2019-06-09 20:17:13 +0900

    add bookmarklet for sp

--- bookmarklet.js
+++ bookmarklet.js
@@ -10,4 +10,17 @@ const bookmarklet_for_pc = (()=>{
     },interval);
 }).toString().replace(/\s+/g," ").replace("\n","");
 
+const bookmarklet_for_sp = (()=>{
+    const interval = 1000;
+    const ID = setInterval(() => {
+        const elem = document.getElementsByClassName("next_button_wrap");
+        if(elem === undefined){
+            clearInterval(ID);
+        }else{
+            elem[0].click();
+        }
+    },interval);
+}).toString().replace(/\s+/g," ").replace("\n","");
+
 document.getElementById("bookmarklet_for_pc").href = `javascrpt:(${bookmarklet_for_pc})()`;
+document.getElementById("bookmarklet_for_sp").href = `javascrpt:(${bookmarklet_for_sp})()`;
